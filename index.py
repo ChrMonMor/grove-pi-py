34e074f3dbdeb4310b3a0713a234604b6f9b9f3a
ChrMonMor <97163196+ChrMonMor@users.noreply.github.com>
2024-09-09 09:02:17 +0200

    Update index.py

--- index.py
+++ index.py
@@ -2,12 +2,15 @@
 ####
 # this the OOP version of the python code in Grove+
 # it needs $ python -m pip install requests : $ python -m pip install json : $ python -m pip install time
-# 
-#
+# timer list represent:  [0] = temp, [1] = humid, [2] = noise, [3] = Air
+# sensorPorts list represent: [0] = temp & humid, [1] = noise, [2] = Air
 ####
+
 import requests
 import json
 import time 
+import math
+import grovepi
 
 class index:
     def __init__(self, sensorPorts) -> None:
@@ -36,17 +39,16 @@ class index:
     def update_pin_timer(self, i) -> None:
         temp = self.get_varibles()
         if i == 0:
-            self.timer[i] = temp[0]['temperature']
+            self.timer[i] = 60 * temp[0]['temperature'] + self.current_time
         if i == 1:
-            self.timer[i] = temp[0]['humidity']
+            self.timer[i] = 60 * temp[0]['humidity'] + self.current_time
         if i == 2:
-            self.timer[i] = temp[0]['noise']
+            self.timer[i] = 60 * temp[0]['noise'] + self.current_time
         if i == 3:
-            self.timer[i] = temp[0]['airquality']
+            self.timer[i] = 60 * temp[0]['airquality'] + self.current_time
     
     # send data off the chosen pin to the cloud database plus the promised time for the next one
-    def send_data(self, i) -> None:
-        readings = self.analogReadSensor(i)
+    def send_data(self, i, readings) -> None:
         self.update_pin_timer(i)
         api_url = "http://192.168.1.147/sensordate.php"
         headers =  {"Content-Type":"application/json"}
@@ -63,7 +65,16 @@ class index:
 
 
     def analogReadSensor(self, pin) -> float:
-        pass
+        return grovepi.analogRead(self.sensorPorts[pin])
+
+    # t == 1 == temp == true
+    # t == 0 == humid == false
+    def dhtRead(self, t) -> float[]:
+        [temp, humid] = grovepi.dht(self.sensorPorts[0])
+        if t == 1:
+            return temp
+        else: 
+            return humid
 
     
 arr = [1,2,3,4]
@@ -72,10 +83,10 @@ main = index(arr)
 while True:
     main.current_time = time.time()
     if main.current_time > main.timer[0]:
-        main.send_data(0)
+        main.send_data(0, main.dhtRead(1))
     if main.current_time > main.timer[1]:
-        main.send_data(1)
+        main.send_data(1, main.dhtRead(0))
     if main.current_time > main.timer[2]:
-        main.send_data(2)
+        main.send_data(2, main.analogReadSensor(1))
     if main.current_time > main.timer[3]:
-        main.send_data(3)
+        main.send_data(3, main.analogReadSensor(2))
